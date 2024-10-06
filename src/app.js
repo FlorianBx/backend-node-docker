a3167749b3554eed04d1abf546122ca2ec51a128
Florian Beaumont <25250981+BFlorian91@users.noreply.github.com>
2024-10-06 15:02:49 +0200

    feat ✨: refresh token system middleware works

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import authRoutes from './routes/auth.js';
 import testRoutes from './routes/tests/testRoutes.js';
+import { tokenRefreshMiddleware } from './middlewares/refreshToken.js';
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
+app.use('/api', tokenRefreshMiddleware);
 app.use('/api/test', testRoutes);
 
 export default app;
